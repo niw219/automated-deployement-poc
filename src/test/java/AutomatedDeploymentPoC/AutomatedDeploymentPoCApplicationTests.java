4a391a9ac3f1169b6210ffeeb76f5cda1ed7c1c7
niw219 <nsw722@gmail.com>
2020-05-01 11:58:37 -0400

    first commit

---
+++ src/test/java/AutomatedDeploymentPoC/AutomatedDeploymentPoCApplicationTests.java
@@ -0,0 +1,13 @@
+package AutomatedDeploymentPoC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatedDeploymentPoCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
